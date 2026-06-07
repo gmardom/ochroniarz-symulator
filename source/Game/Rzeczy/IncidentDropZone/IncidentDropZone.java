53e85d8459ea88db28a125ec060f7cc92f6290c6
Dominik Marcinowski <dmarcinowski@zoho.com>
2026-06-07 18:45:39 +0200

    feat(game): stwórz IncidentDropZone (Area3D) dla dostarczania ogłuszonych

---
+++ source/Game/Rzeczy/IncidentDropZone/IncidentDropZone.java
@@ -0,0 +1,36 @@
+package Game.Rzeczy.IncidentDropZone;
+
+import Player.Player;
+import godot.annotation.*;
+import godot.api.*;
+import godot.core.*;
+
+@RegisterClass
+public class IncidentDropZone extends Area3D
+{
+    @RegisterFunction
+    public void _ready()
+    {
+        connect("body_entered", new NativeCallable(this, new StringName("onBodyEntered")));
+        connect("body_exited", new NativeCallable(this, new StringName("onBodyExited")));
+    }
+
+    @RegisterFunction
+    public void onBodyEntered(Node body)
+    {
+        if (body instanceof Player player) {
+            player.setInDropZone(true);
+            if (player.isDragging()) {
+                player.dropDeliver();
+            }
+        }
+    }
+
+    @RegisterFunction
+    public void onBodyExited(Node body)
+    {
+        if (body instanceof Player player) {
+            player.setInDropZone(false);
+        }
+    }
+}
